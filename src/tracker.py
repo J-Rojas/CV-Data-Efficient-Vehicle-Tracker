bff05d15bc343fa46d77383f1af6cd31db8f6c33
Jose Rojas <jrojas@redlinesoftworks.com>
2025-03-22 06:20:58 +0000

    Add various keyframe tracking

--- src/tracker.py
+++ src/tracker.py
@@ -138,17 +138,14 @@ class TrackingObject():
         return pixels
 
     def get_sizes_as_array(self, idxes=None):        
-        regions = self.regions.keys()
-        min_idx = min(regions)
-        max_idx = max(regions)
-        arr = np.zeros((max_idx - min_idx + 1, 2))
+        arr = np.zeros((len(self.regions), 2))
         for i, region_list in enumerate(self.regions.values()):
             region_arr = np.array(region_list)
             min_region = region_arr.min(axis=0)
             max_region = region_arr.max(axis=0)
             arr[i,:] = [abs(min_region[0] - max_region[2]), abs(min_region[1] - max_region[3])]
         
-        return arr, min_idx, max_idx
+        return arr
 
     def region_offset(self, reference_frame, target_frame):
         pixels = self.get_containing_pixels(target_frame)        
@@ -157,7 +154,7 @@ class TrackingObject():
 class Tracker():
 
     SIMILARITY_THRESHOLD = 0.2
-    SLIDING_WINDOW_SIZE = 13
+    SLIDING_WINDOW_SIZE = 3
     REGION_FILTER_WINDOW_SIZE = 5
     REGION_FILTER_THRESHOLD = 0.8
     REGION_MIN_DIMENSION = 15
@@ -190,10 +187,12 @@ class Tracker():
         return best, np.array(np.unravel_index(pos, scores.shape)) * (-1 if swap else 1)
 
     def filter_detection_masks(self):
-        # perform a sliding window filtering with an a correspondence process and image difference to denoise the detection regions 
+        # The detector will usually overestimate the detections and some noisy pixels near the borders
+        # will cause trouble with accurate size tracking. 
+        # perform a sliding window filtering with an a correspondence process and image difference to denoise the detection regions. 
         # This is a destructive process. The proposal pixel_values and regions are eliminated and a single coalesced and filtered pixel_values and region are generated per frame per object
 
-        for i in range(2):
+        for i in range(self.REGION_FILTER_WINDOW_SIZE // 2):
             
             for obj in self.objects:
         
@@ -220,23 +219,23 @@ class Tracker():
                         if k == key:
                             continue
                         # determine the cross correlation for the top-left and bottom-right corners and get the padding for this frame
-                        scores_tl.append(self.best_scores(patch_matching_cross_correlation(ref, im)))
-                        scores_br.append(self.best_scores(patch_matching_cross_correlation(ref_flipped, np.flip(im, axis=(0, 1)))))
+                        scores_tl.append(self.best_scores(patch_matching_cross_correlation(ref, im, use_convolve=False)))
+                        scores_br.append(self.best_scores(patch_matching_cross_correlation(ref_flipped, np.flip(im, axis=(0, 1)), use_convolve=False)))
                     
                     # filter scores, padding only, not considering "growing" by using negative values
-                    print(scores_tl)
-                    print(scores_br)
+                    #print(scores_tl)
+                    #print(scores_br)
                     offsets_tl = np.array(list(map(lambda x: np.clip(x[1], 0, np.inf), filter(lambda x: x[0] > self.REGION_FILTER_THRESHOLD, scores_tl))))
                     offsets_br = np.array(list(map(lambda x: np.clip(x[1], 0, np.inf), filter(lambda x: x[0] > self.REGION_FILTER_THRESHOLD, scores_br))))
                     
-                    print(offsets_tl)
-                    print(offsets_br)                
+                    #print(offsets_tl)
+                    #print(offsets_br)                
                     
                     pad_tl = np.ceil(offsets_tl.mean(axis=0)).astype(np.int32)
                     pad_br = np.ceil(offsets_br.mean(axis=0)).astype(np.int32)
 
-                    print(pad_tl)
-                    print(pad_br)                
+                    #print(pad_tl)
+                    #print(pad_br)                
                     
                     if pad_tl.size < 2:
                         pad_tl = [0, 0]
@@ -327,7 +326,7 @@ class Tracker():
         
         return im, pred_mask, logits, loss
 
-    def determine_tracking_trajectories(self) -> tuple[dict[int, dict[int, tuple]], dict[int, dict[int, tuple]]]:
+    def find_good_keyframes_median(self, obj):
 
         conv_filter = np.ones(self.SLIDING_WINDOW_SIZE) / self.SLIDING_WINDOW_SIZE
 
@@ -338,44 +337,135 @@ class Tracker():
             bboxes = tracking_bboxes[obj.num_id] = {}
             cor_bboxes = correction_bboxes[obj.num_id] = {}
 
-            # determine tracking stability by considering region sizes - stable sizes == good detections
-            sizes, _, _ = obj.get_sizes_as_array() # note: there can be missing frames in this list
+        # determine tracking stability by considering region sizes - stable sizes == good detections
+        sizes = obj.get_sizes_as_array() # note: there can be missing frames in this list
+        min_frame = min(obj.regions.keys())
+        max_frame = max(obj.regions.keys())
+
+        if sizes.shape[0] < self.SLIDING_WINDOW_SIZE:
+            # object does not have enough detections to consider
+            return None
+
+        # convolve over a window to determine the sliding window mean
+        sizes_median = sliding_window_median(sizes.T, (1, self.SLIDING_WINDOW_SIZE))
+        #sizes_mean = np.apply_along_axis(lambda x: np.convolve(x, conv_filter, mode="same"), 1, sizes.T)
+
+        # toss outliers and compute the averaged size in the window
+
+        pad_left = (self.SLIDING_WINDOW_SIZE - 1) // 2
+        pad_right = self.SLIDING_WINDOW_SIZE - 1 - pad_left
+
+        arr_padded = np.pad(sizes.T, ((0, 0), (pad_left, pad_right)), mode='constant')
+
+        windows = sliding_window_view(arr_padded, (1, self.SLIDING_WINDOW_SIZE))
+
+        # Compute the standard deviation over the last two axes (the window dimensions)
+        
+        std_map = windows.std(axis=(-2, -1)) * 0.25
+        valid_items = np.all(
+            (sizes.T >= sizes_median - std_map) & (sizes.T <= sizes_median + std_map),
+            axis=0
+        )
+        keyframes = np.where(valid_items)[0]
+        
+        sizes_mean_kf = np.round(np.apply_along_axis(lambda x: np.convolve(x, conv_filter, mode="same"), 1, sizes.T[:,keyframes]).T if len(keyframes) > 0 else np.array([]))
+
+        keyframes_nums = list(filter(lambda x: x in obj.regions.keys(), keyframes + min_frame))
+
+        if len(keyframes) == 0:
+            # object is too unstable to consider
+            return None
+
+        return keyframes_nums, sizes_mean_kf
+            
+    def find_good_keyframes_interp(self, obj):
+
+        conv_filter = np.ones(self.SLIDING_WINDOW_SIZE)
+        
+        sizes = obj.get_sizes_as_array() # note: there can be missing frames in this list
+        keys = list(obj.frame_pixels.keys())
+
+        min_frame = min(obj.regions.keys())
+        max_frame = max(obj.regions.keys())
+
+
+        if sizes.shape[0] < self.SLIDING_WINDOW_SIZE:
+            # object does not have enough detections to consider
+            return None
+
+        # calculate the gradient of the sizes
+        diff_1st = np.diff(sizes, 1, axis=0)
+        #diff_2nd = np.diff(sizes, 2, axis=0)
+        filtered_diff = np.apply_along_axis(lambda x: np.convolve(x, conv_filter, mode="same"), 1, diff_1st)
+
+        # use the frames where the sizes are stable
+        valid_items = np.where(
+            (np.abs(filtered_diff[:,0]) < sizes[1:,0] * 0.1) & (np.abs(filtered_diff[:,1]) < sizes[1:,1] * 0.1)            
+        )[0]
+        invalid_items = np.where(
+            (np.abs(filtered_diff[:,0]) >= sizes[1:,0] * 0.1) | (np.abs(filtered_diff[:,1]) >= sizes[1:,1] * 0.1)            
+        )[0]
+
+        print(valid_items)
+
+        if len(valid_items) == 0:
+            # object is too unstable to consider
+            return None
+
+        valid_keys = [keys[i] for i in valid_items]
+        invalid_keys = [keys[i] for i in invalid_items]
+        
+        # linearly interpolate
+        sizes_valid = sizes[valid_items,:]               
+        sizes_filtered = np.zeros((max_frame - min_frame + 1, 2))
+        sizes_filtered[np.array(valid_keys) - min_frame] = sizes_valid
+        
+        # this seems tough to vectorize.... might need to rethink the approach
+        if len(invalid_items) > 0:
+            for i in range(len(invalid_items)):           
+                if len(valid_items) == 1:
+                    sizes_filtered[invalid_keys[i] - min_frame] = sizes[valid_items[0]]
+                else: 
+                    last_valid_idx = bisect_left(valid_items, invalid_items[i])
+                    last_valid = valid_items[last_valid_idx-1]
+                    if last_valid_idx < len(valid_items):
+                        next_valid = valid_items[last_valid_idx]
+                    else:
+                        # special case if invalid is after all valid frames
+                        sizes_filtered[invalid_keys[i] - min_frame] = sizes[-1]
+                        continue
+                
+                    sizes_filtered[invalid_keys[i] - min_frame] = (sizes[next_valid] - sizes[last_valid]) / (next_valid - last_valid) * (invalid_items[i] - last_valid) + sizes[last_valid]              
+
+        return valid_keys, sizes_filtered
+
+    def find_good_keyframes(self, obj):
+
+        return self.find_good_keyframes_interp(obj)
+
+
+    def determine_tracking_trajectories(self) -> tuple[dict[int, dict[int, tuple]], dict[int, dict[int, tuple]]]:
+
+        
+
+        tracking_bboxes = {}
+        correction_bboxes = {}
+        for obj in self.objects:
+
             min_frame = min(obj.regions.keys())
+            max_frame = max(obj.regions.keys())
 
-            if sizes.shape[0] < self.SLIDING_WINDOW_SIZE:
-                # object does not have enough detections to consider
+            bboxes = tracking_bboxes[obj.num_id] = {}
+            cor_bboxes = correction_bboxes[obj.num_id] = {}
+
+            results = self.find_good_keyframes(obj)
+
+            if results is None:
                 continue
 
-            # convolve over a window to determine the sliding window mean
-            sizes_mean = np.apply_along_axis(lambda x: np.convolve(x, conv_filter, mode="same"), 1, sizes.T)
-
-            # toss outliers and compute the averaged size in the window
-
-            pad_left = (self.SLIDING_WINDOW_SIZE - 1) // 2
-            pad_right = self.SLIDING_WINDOW_SIZE - 1 - pad_left
-
-            arr_padded = np.pad(sizes.T, ((0, 0), (pad_left, pad_right)), mode='constant')
-
-            windows = sliding_window_view(arr_padded, (1, self.SLIDING_WINDOW_SIZE))
-
-            # Compute the standard deviation over the last two axes (the window dimensions)
-            
-            std_map = windows.std(axis=(-2, -1))
-            valid_items = np.all(
-                (sizes.T >= sizes_mean - std_map) & (sizes.T <= sizes_mean + std_map),
-                axis=0
-            )
-            keyframes = np.where(valid_items)[0]
-            
-            sizes_mean_kf = np.round(np.apply_along_axis(lambda x: np.convolve(x, conv_filter, mode="same"), 1, sizes.T[:,keyframes]).T if len(keyframes) > 0 else np.array([]))
-
-            keyframes_nums = list(filter(lambda x: x in obj.regions.keys(), keyframes + min_frame))
-
-            if len(keyframes) == 0:
-                # object is too unstable to consider
-                continue
-            
-            occluded_frames = list(filter(lambda x: x not in obj.regions.keys(), keyframes + min_frame))
+            keyframes_nums, sizes_mean_kf = results
+        
+            occluded_frames = list(filter(lambda x: x not in obj.regions.keys(), range(min_frame, max_frame + 1)))
 
             for idx, _ in obj.frame_pixels.items():
 
@@ -385,31 +475,30 @@ class Tracker():
                 #keyframe_next = keyframes_nums[keyframe_idx]
                 keyframe = keyframe_prev #if abs(keyframe_prev - idx) <= abs(keyframe_next - idx) else keyframe_next                
                 
-                if abs(keyframe - idx) <= self.SLIDING_WINDOW_SIZE // 2:
+                bbox = obj.get_containing_region(idx)
+                bboxes[idx] = bbox
 
-                    bbox = obj.get_containing_region(idx)
-                    bboxes[idx] = bbox                    
-                    # find a representative region in the keyframe
-                    #bbox = obj.get_containing_region(keyframe)
-                    (y_off, x_off) = obj.region_offset(keyframe, idx)
-                    print(f"key {idx}, ", bbox)
-                    print(idx, keyframe, y_off, x_off, sizes_mean_kf[keyframe_idx - 1, 0], sizes_mean_kf[keyframe_idx - 1, 1])
-                    # reposition and apply the mean width and height
-                    cor_bboxes[idx] = (int(bbox[0] - y_off), int(bbox[1] - x_off), int(bbox[2]), int(bbox[3])) # - y_off + sizes_mean_kf[keyframe_idx - 1, 0]), int(bbox[1] - x_off + sizes_mean_kf[keyframe_idx - 1, 1]))                    
-                else: 
-                    
-                    # the object is likely occluded, check if the object exists in both the previous and next frame
-                    if obj.regions.get(max(0, idx - 1)) is not None and \
-                       obj.regions.get(min(len(obj.regions) - 1, idx + 1)) is not None:
-                        
-                        # mark the object and frame for coalescing as post-processing step
-                        occluded_frames.append(idx)
+                # find a representative region in the keyframe
+                #bbox = obj.get_containing_region(keyframe)
+                (y_off, x_off) = obj.region_offset(keyframe, idx)
+                print(f"key {idx}, ", bbox)
+                print(idx, keyframe, y_off, x_off, sizes_mean_kf[idx - min_frame, 0], sizes_mean_kf[idx - min_frame, 1])
+                # reposition and apply the mean width and height
+                cor_bboxes[idx] = (int(bbox[0] - y_off), int(bbox[1] - x_off), int(bbox[0] - y_off + sizes_mean_kf[idx - min_frame, 0]), int(bbox[1] - x_off + sizes_mean_kf[idx - min_frame, 1]))                                    
             
             for idx in occluded_frames:
 
-                # no regions to compare, just make a best estimate
+                # no regions to compare, just make a best estimate            
                 prev = obj.get_containing_region(idx - 1)
                 next = obj.get_containing_region(idx + 1)
+
+                if next is None:
+                    next = prev
+                if prev is None:
+                    prev = next
+                if prev is None:
+                    # uh-oh
+                    assert(False)
 
                 bbox = np.stack([prev, next]).mean(axis=0).astype(np.int32)
 
@@ -525,10 +614,10 @@ if __name__ == '__main__':
             cv2.putText(im, str(0), (x1, y2), cv2.FONT_HERSHEY_PLAIN, 1.0, (255, 255, 0))
             cv2.rectangle(im, (x1, y1), (x2, y2), color=(0, 0, 255), thickness=2)
 
-            if corr_bbox:
+            if corr_bbox is not None:
                 corr_bbox = rescale_bboxes(tracker.detector.IMAGE_OUTPUT_SIZE, OUTPUT_SIZE, np.array([corr_bbox]))[0]        
                 y1, x1, y2, x2 = corr_bbox                
-                #cv2.rectangle(im, (x1, y1), (x2, y2), color=(255, 0, 0), thickness=2)
+                cv2.rectangle(im, (x1, y1), (x2, y2), color=(255, 0, 0), thickness=2)
             
         else:
             ious.append(0)
